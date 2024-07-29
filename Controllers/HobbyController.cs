a87c5db5994131df23be5b86aea85b237060c3ce
tordvb10 <tordvn10.kodehode@gmail.com>
2024-07-29 14:05:35 +0200

    finnished-for-today.

--- Controllers/HobbyController.cs
+++ Controllers/HobbyController.cs
@@ -20,5 +20,31 @@ namespace TestingHTTPie.Controllers
             _hobbyRepository = hobbyRepository;
             _mapper = mapper;
         }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<HobbyDto>))]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> GetHobbies()
+        {
+            var hobbies = _mapper.Map<List<HobbyDto>>(await _hobbyRepository.GetHobbiesAsync());
+            return (!ModelState.IsValid) ? BadRequest(ModelState) : Ok(hobbies);
+        }
+
+
+        [HttpGet("{hobbyId}")]
+        [ProducesResponseType(200, Type = typeof(HobbyDto))]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> GetHobby(Guid hobbyId)
+        {
+            if (!await _hobbyRepository.HobbyExistsAsync(hobbyId)) return NotFound();
+            var hobbyDto = _mapper.Map<HobbyDto>(await _hobbyRepository.GetHobbyAsync(hobbyId));
+            if (!ModelState.IsValid) return BadRequest(ModelState);
+            return Ok(hobbyDto);
+        }
+
     }
 }
