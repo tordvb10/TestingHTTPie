1eec127a9725f6852d79776c687fdb7b4090d725
tordvb10 <tordvn10.kodehode@gmail.com>
2024-07-29 12:09:26 +0200

    gettingreaddy-to-'helper'

---
+++ Dto/HobbyDto.cs
@@ -0,0 +1,12 @@
+namespace TestingHTTPie.Dto
+{
+    public class HobbyDto
+    {
+        public Guid Id { get; set; }
+        public string Activity { get; set; }
+
+        public ICollection<HobbyPersonDto> HobbyPersonDtos { get; set; }
+
+
+    }
+}
---
+++ Dto/PersonDto.cs
@@ -0,0 +1,13 @@
+namespace TestingHTTPie.Dto
+{
+    public class PersonDto
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public ICollection<HobbyPersonDto> HobbyPersonDtos { get; set; }
+
+
+    }
+}
