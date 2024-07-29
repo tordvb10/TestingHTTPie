84ccd1256324c07c41b1f5c9fb1def602aa5ae1d
tordvb10 <tordvn10.kodehode@gmail.com>
2024-07-29 13:43:49 +0200

    first-cotroller-ready-with-to-inject-implementations

---
+++ Controllers/HobbyController.cs
@@ -0,0 +1,17 @@
+using AutoMapper;
+using TestingHTTPie.Dto;
+using TestingHTTPie.Interfaces;
+using TestingHTTPie.Models;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace TestingHTTPie.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HobbyController : Controller
+    {
+
+    }
+}
