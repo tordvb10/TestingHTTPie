67a9a4c98c9a09caee1856aa0c7d8e622a16aae9
tordvb10 <tordvn10.kodehode@gmail.com>
2024-07-29 12:16:12 +0200

    implemented-first-repository

---
+++ Repositories/HobbyRepository.cs
@@ -0,0 +1,43 @@
+﻿using TestingHTTPie.Interfaces;
+using TestingHTTPie.Models;
+
+namespace TestingHTTPie.Repositories
+{
+    public class HobbyRepository : IHobbyRepository
+    {
+        public Task<bool> CreateHobbyAsync(Hobby Hobby)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteHobbyAsync(Hobby Hobby)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<Hobby>> GetHobbiesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Hobby> GetHobbyAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> HobbyExistsAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> SaveAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> UpdateHobbyAsync(Hobby Hobby)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
