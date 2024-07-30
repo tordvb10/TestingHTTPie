6aabb142ab506071d86c40fc826c5647bea164b5
tordvb10 <tordvn10.kodehode@gmail.com>
2024-07-30 11:32:52 +0200

    halfwaytherewithcontrollerhobby

--- Controllers/HobbyController.cs
+++ Controllers/HobbyController.cs
@@ -28,10 +28,37 @@ namespace TestingHTTPie.Controllers
         [ProducesResponseType(400)]
         public async Task<IActionResult> GetHobbies()
         {
-            var hobbies = _mapper.Map<List<HobbyDto>>(await _hobbyRepository.GetHobbiesAsync());
-            return (!ModelState.IsValid) ? BadRequest(ModelState) : Ok(hobbies);
+            var getHobbiesDto = _mapper.Map<ICollection<Hobby>, List<HobbyDto>>(await _hobbyRepository.GetHobbiesAsync());
+            return (!ModelState.IsValid) ? BadRequest(ModelState) : Ok(getHobbiesDto);
         }
 
+        [HttpPost]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(500)]
+        [ProducesResponseType(404)]
+        [ProducesResponseType(400)]
+
+        public async Task<IActionResult> CreateHobby([FromBody] HobbyDto createHobbyDto)
+        {
+            if (createHobbyDto == null | !ModelState.IsValid) return BadRequest(ModelState);
+            var createHobbyExists = (await _hobbyRepository.GetHobbiesAsync())
+                .Where(h => h.Activity == createHobbyDto.Activity)
+                .FirstOrDefault();
+            if (createHobbyExists != null)
+            {
+                ModelState.AddModelError("", "Employee already exists.");
+                return StatusCode(422, ModelState);
+            }
+            var createHobby = _mapper.Map<HobbyDto, Hobby>(createHobbyDto);
+            if (!await _hobbyRepository.CreateHobbyAsync(createHobby))
+            {
+                ModelState.AddModelError("", "Something went wrong while saving.");
+                return StatusCode(500, ModelState);
+            }
+            return Ok("Successfully created.");
+        }
+
+        //[HttpDelete]
 
         [HttpGet("{hobbyId}")]
         [ProducesResponseType(200, Type = typeof(HobbyDto))]
@@ -41,9 +68,15 @@ namespace TestingHTTPie.Controllers
         public async Task<IActionResult> GetHobby(Guid hobbyId)
         {
             if (!await _hobbyRepository.HobbyExistsAsync(hobbyId)) return NotFound();
-            var hobbyDto = _mapper.Map<HobbyDto>(await _hobbyRepository.GetHobbyAsync(hobbyId));
-            if (!ModelState.IsValid) return BadRequest(ModelState);
-            return Ok(hobbyDto);
+            var getHobbyDto = _mapper.Map<Hobby,HobbyDto>(await _hobbyRepository.GetHobbyAsync(hobbyId));
+            return (!ModelState.IsValid) ? BadRequest(ModelState) : Ok(getHobbyDto);
+        }
+
+        [HttpPut("{hobbyId}")]
+        [ProducesResponseType(400)]
+        public async Task<IActionResult> UpdateHobby(Guid hobbyId, [FromBody] HobbyDto updateHobbyDto)
+        {
+            return NoContent();
         }
 
     }
